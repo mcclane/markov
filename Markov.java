565eef8a2fb731e2291dbceafb1e8da34f471265
McClane Howland <mcclane.howland@gmail.com>
2017-10-08 19:45:27 -0700

    created sources file

--- Markov.java
+++ Markov.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class Markov {
     public static void main(String[] args) {
-        Generator gen = new Generator("processedTweets.txt");
+        Generator gen = new Generator("sources/processedTweets.txt");
         gen.generateMap(2);
 
         Scanner input = new Scanner(System.in);
@@ -20,59 +20,5 @@ public class Markov {
             System.out.println(gen.generateText(seed, 500));
             System.out.println();
         }
-
-        /*HashMap<String, HashSet<String>> wc = new HashMap<String, HashSet<String>>();
-        ArrayList<String> text = new ArrayList<String>();
-        try {
-            Scanner sc = new Scanner(new File("source.txt"));
-            //read in the file
-            while(sc.hasNext()) {
-                text.add(sc.next());
-            }
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-
-        //generate prefix/suffix frequency map
-        int len = text.size();
-        for(int i = 0;i < text.size()-2;i++) {
-            String prefix = text.get(i)+" "+text.get(i+1);
-            String suffix = text.get(i+2);
-            //check if the suffix is already in the map
-            HashSet<String> internal;
-            if(wc.containsKey(prefix)) {
-                internal = wc.get(prefix);
-            }
-            else {
-                internal = new HashSet<String>();
-            }
-            internal.add(suffix);
-            wc.put(prefix, internal);
-        }
-        //now that we have a frequency map, we can create a markov chain
-        Scanner ip = new Scanner(System.in);
-        int limit = 100; //words
-        while(true) {
-            System.out.println("Enter two seed words: ");
-            String prefix = ip.nextLine().trim();
-            System.out.print(prefix+" ");
-            for(int i = 0;i < limit;i++) {
-                //find a random word from the internal suffix
-                HashSet<String> internal = wc.get(prefix);
-                String suffix = "";
-                int randIndex = (int)(Math.random()*internal.size());
-                int randCounter = 0;
-                for(String k : internal) {
-                    if(randIndex == randCounter) {
-                        suffix = k;
-                        break;
-                    }
-                    randCounter++;
-                }
-                System.out.print(suffix+" ");
-                prefix = prefix.split(" ")[1]+" "+suffix;
-            }
-        }*/
     }
-
 }
