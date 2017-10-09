472e8b46c8b317b1332671e692b89cf3e023d6ab
McClane Howland <mcclane.howland@gmail.com>
2017-10-08 19:20:04 -0700

    added another text source

--- Markov.java
+++ Markov.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class Markov {
     public static void main(String[] args) {
-        Generator gen = new Generator("source.txt");
+        Generator gen = new Generator("processedTweets.txt");
         gen.generateMap(2);
 
         Scanner input = new Scanner(System.in);
